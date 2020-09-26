ef2d8be7194cd1d51e68f2573f5d4279802257df
fatsoflo <flo.fong.6@gmail.com>
2020-09-26 13:29:38 -0400

    new ali branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DEVELOP BRANCH
+          ALI BRANCH
         </p>
       </header>
     </div>
