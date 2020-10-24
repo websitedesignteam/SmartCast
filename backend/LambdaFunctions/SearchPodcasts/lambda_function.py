596e83c741e0ba597625b4535d7e6a05c91ab785
Ahsan Fayyaz <ahsan@iknox.com>
2020-10-24 09:30:36 +0000

    Finished implementing &  testing the searchPodcast lambda

--- backend/LambdaFunctions/SearchPodcasts/lambda_function.py
+++ backend/LambdaFunctions/SearchPodcasts/lambda_function.py
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
         returnData = {}
         
         url = "https://listen-api.listennotes.com/api/v2/search"
-        querystring = {"sort_by_date":"0", "type":"podcast","offset":"0","len_min":"2","len_max":"10","genre_ids":"68%2C82","published_before":"1490190241000","published_after":"1390190241000","only_in":"title","language":"English","safe_mode":"1","q":searchString}
+        querystring = {"sort_by_date":"0", "type":"podcast","offset":"0","len_min":"2","len_max":"10","only_in":"title","language":"English","safe_mode":"1","q":searchString}
         
         headers = {
             'X-ListenAPI-Key': os.environ.get("APIKEY")
@@ -36,8 +36,30 @@ def lambda_handler(event, context):
         response = requests.request("GET", url, headers=headers, params=querystring)
         responseData = response.json()
         print(responseData)
+        
+        #generic data for the podcastsearch
+        returnData["totalCount"] = responseData["count"]
+        returnData["totalPodcasts"] = responseData["total"]
+        
+        #specific data for each podcast
+        returnData["podcasts"] = []
+        podcastList = responseData["results"]
+        
+        for podcast in podcastList:
+            podcastObj = {}
+            podcastObj["podcastID"] = podcast["id"]
+            podcastObj["podcastTitle"] = podcast["title_original"]
+            podcastObj["totalEpisodes"] = podcast["total_episodes"]
+            podcastObj["podcastURL"] = podcast["listennotes_url"]
+            podcastObj["podcastPublisher"] = podcast["publisher_original"]
+            podcastObj["podcastDescription"] = podcast["description_original"]
+            podcastObj["podcastImage"] = podcast["image"]
+            podcastObj["podcastThumbnail"] = podcast["thumbnail"]
+            podcastObj["podcastGenreIDs"] = podcast["genre_ids"]
+            returnData["podcasts"].append(podcastObj)
+        
         return {
-            "Data": responseData
+            "Data": returnData
         }
 
 
