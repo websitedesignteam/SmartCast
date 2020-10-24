72dc015dea0e5f12e00dc9a335b6a97c861abc10
Ahsan Fayyaz <ahsan@iknox.com>
2020-10-24 08:01:58 +0000

    implemented the SearchEpisodes lambda which takes in a user query string as input

--- backend/LambdaFunctions/SearchEpisodes/lambda_function.py
+++ backend/LambdaFunctions/SearchEpisodes/lambda_function.py
@@ -1,8 +1,6 @@
 import json
 import requests
 import os
-import boto3
-import time
 
 
 def lambda_handler(event, context):
@@ -12,80 +10,61 @@ def lambda_handler(event, context):
     body = json.loads(body)
     
     #Extract values from GET request and initialize vars for function call
-    queryString =str(body["queryString"])
+    searchString =str(body["searchString"])
     
     if "sortBy" in body:
         sortBy = str(body["sortBy"])
     else:
         sortBy = None
-    
-    if "nextPage" in body:
-        nextPage = str(body["nextPage"])
-    else:
-        nextPage = None
-        
         
         
     #search Function
-    def searchEpisodes(queryString):
+    def searchEpisodes(searchString, sortBy=None):
         
         if sortBy is None:
             sortBy = "recent_first"
-        
+            
         returnData = {}
         
-        if nextPage is not None:
-            nextPub = "next_episode_pub_date=" + str(nextPage) + "&"
-            
-        else:
-            nextPub = ""
-            
-            
-        url = "https://listennotes.p.rapidapi.com/api/v1/search"
-        querystring = {"sort_by_date":"0","type":"episode","offset":"0","len_min":"2","len_max":"10","genre_ids":"68%2C82","published_before":"1490190241000","published_after":"1390190241000","only_in":"title","language":"English","safe_mode":"1","q":queryString}
+        url = "https://listen-api.listennotes.com/api/v2/search"
+        querystring = {"sort_by_date":"0", "type":"episode","offset":"0","len_min":"2","len_max":"10","genre_ids":"68%2C82","published_before":"1490190241000","published_after":"1390190241000","only_in":"title","language":"English","safe_mode":"1","q":searchString}
         
         headers = {
-            'x-rapidapi-host': "listennotes.p.rapidapi.com",
             'X-ListenAPI-Key': os.environ.get("APIKEY")
         }
+        
         response = requests.request("GET", url, headers=headers, params=querystring)
+        responseData = response.json()
+        print(responseData)
+        #generic data for the search
+        returnData["totalCount"] = responseData["count"]
+        returnData["totalEpisodes"] = responseData["total"]
+        # returnData["nextPageNumber"] = responseData["next_episode_pub_date"]
         
-        data = response.json()
-        print(response)
-        
-        
-        
-        
-        
-        print(data)
-        returnData["podcastID"] = data["id"]
-        returnData["podcastTitle"] = data["title"]
-        returnData["podcastPublisher"] = data["publisher"]
-        returnData["podcastImage"] = data["image"]
-        returnData["podcastThumbnail"] = data["thumbnail"]
-        returnData["podcastTotalEpisdoes"] = data["total_episodes"]
-        returnData["podcastDescription"] = data["description"]
-        returnData["genreIDs"] = data["genre_ids"]
-        returnData["nextPageNumber"] = data["next_episode_pub_date"]
+        #specific data for each episode
         returnData["episodes"] = []
-        podcasts = data["episodes"]
-        for pod in podcasts:
-            obj = {}
-            obj["episodeID"] = pod["id"]
-            obj["episodeTitle"] = pod["title"]
-            obj["episodeImage"] = pod["image"]
-            obj["episodeThumbnail"] = pod["thumbnail"]
-            obj["episodeLengthSeconds"] = pod["audio_length_sec"]
-            returnData["episodes"].append(obj)
+        episodeList = responseData["results"]
+        
+        for episode in episodeList:
+            episodeObj = {}
+            episodeObj["episodeID"] = episode["id"]
+            episodeObj["episodeTitle"] = episode["title_original"]
+            episodeObj["podcastID"] = episode["podcast"]["id"]
+            episodeObj["podcastTitle"] = episode["podcast"]["title_original"]
+            episodeObj["episodeDescription"] = episode["description_original"]
+            episodeObj["episodeImage"] = episode["image"]
+            episodeObj["episodeAudioLink"] = episode["audio"]
+            episodeObj["audioLength"] = episode["audio_length_sec"]
+            returnData["episodes"].append(episodeObj)
         
         print(returnData)
         return {
             "Data": returnData
         }
 
-    # TODO implement
+
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(getAllEpisodes(podcastID = podcastID,sortBy = sortBy, nextPage = nextPage))
+        'body': json.dumps(searchEpisodes(searchString))
     }
