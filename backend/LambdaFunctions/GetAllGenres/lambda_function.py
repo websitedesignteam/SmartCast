90a5cf7e1c5db9d264385956e6fc35b66b198f9e
arun-ajay <arunajay42795@gmail.com>
2020-10-31 06:04:04 +0000

    Eliminated nested function from /getallgenres and added specific error codes for portions of the code

--- backend/LambdaFunctions/GetAllGenres/lambda_function.py
+++ backend/LambdaFunctions/GetAllGenres/lambda_function.py
@@ -5,34 +5,49 @@ import requests
 def lambda_handler(event, context):
     
     #Main function call
-    def getAllGenres():
-        try:
-            #Listennotes API CALL - GET /genres
-            url = 'https://listen-api.listennotes.com/api/v2/genres?top_level_only=0'
-            headers = {
-              'X-ListenAPI-Key': os.environ.get("APIKEY"),
-            }
-            response = requests.request('GET', url, headers=headers)
-            data = response.json()
-            genres = data["genres"]
+    try:
+        #Listennotes API CALL - GET /genres
+        url = 'https://listen-api.listennotes.com/api/v2/genres?top_level_only=0'
+        headers = {
+          'X-ListenAPI-Key': os.environ.get("APIKEY"),
+        }
+        response = requests.request('GET', url, headers=headers)
+        data = response.json()
+        genres = data["genres"]
+        
+        hash_genres = {}
+        for genre in genres:
+            hash_genres[genre["name"]] = genre["id"]
             
-            hash_genres = {}
-            for genre in genres:
-                hash_genres[genre["name"]] = genre["id"]
-                
-            
-            print(hash_genres)
-            return {
-                "Data": hash_genres
-            }
-        except Exception as e:
-            print(str(e))
-            return {
-                "Data": {}
-            }
+        
+        data = {
+            "Data": hash_genres
+        }
+
+    except Exception as e:
+        print(str(e))
+        message = {
+            "Error": "Unable to fetch data from Listen Notes"
+        }
+        return {
+            'statusCode': 503,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'},
+            'body': json.dumps(message)
+        }
+
     # TODO implement
     return {
         'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json.dumps(getAllGenres())
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'},
+        'body': json.dumps(data)
     }
