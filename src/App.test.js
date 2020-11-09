74e617d63c519678ac8331ec703050c7d288f190
fatsoflo <flo.fong.6@gmail.com>
2020-11-08 21:55:01 -0500

    update test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('full app rendering/navigating', () => {
+  render(<App />)
+  // verify page content for expected route
+  expect(screen.getByText(/SmartCast/i)).toBeInTheDocument()
+})
