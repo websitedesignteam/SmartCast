e4b9f967b057a13f4d22076b028f17d57f47f2e4
arun-ajay <arunajay42795@gmail.com>
2020-11-16 04:46:52 +0000

    added exception for expired codes

--- backend/LambdaFunctions/ConfirmResetPassword/lambda_function.py
+++ backend/LambdaFunctions/ConfirmResetPassword/lambda_function.py
@@ -107,12 +107,26 @@ def lambda_handler(event, context):
         },
         'body': json.dumps(body)
         } 
-    
+    except client.exceptions.ExpiredCodeException as e:
+        body = {
+            "Success": "Your code has expired. Please request for a new one."
+        }
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
+        
     except Exception as e:
         body = {
             "Success": "Something went wrong. Please check back at a later time."
         }
-        print(str(e))
         return {
             'statusCode': 500,
             'headers': {
