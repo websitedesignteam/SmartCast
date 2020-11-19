0a519eb3cc49cb3c7a48d9fb578dec6f50a68482
arun-ajay <arunajay42795@gmail.com>
2020-11-19 17:03:24 +0000

    added invalid password exception handling for resetting password

--- backend/LambdaFunctions/ConfirmResetPassword/lambda_function.py
+++ backend/LambdaFunctions/ConfirmResetPassword/lambda_function.py
@@ -72,6 +72,21 @@ def lambda_handler(event, context):
             },
             'body': json.dumps(body)
         }
+    except client.exceptions.InvalidPasswordException as e:
+        body = {
+            "Error": "Please ensure that your password has at least 8 characters and contains a mix of uppercase letters, lowercase letters, special characters and numbers."
+        }
+        return {
+        'statusCode': 400,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        },
+        'body': json.dumps(body)
+        } 
     except client.exceptions.CodeMismatchException as e:
         body = {
             "Error": "You have provided an invalid code."
