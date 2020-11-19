3baa8541cab1fbdd7eb6797f99d395d86545de00
arun-ajay <arunajay42795@gmail.com>
2020-11-19 17:02:21 +0000

    added invalid password exception handling for signup

--- backend/LambdaFunctions/SignUp/lambda_function.py
+++ backend/LambdaFunctions/SignUp/lambda_function.py
@@ -105,6 +105,22 @@ def lambda_handler(event, context):
             },
             'body': json.dumps(body)
         }
+    except client.exceptions.InvalidPasswordException as e:
+        body = {
+            "Error": "Please ensure that your password has at least 8 characters and contains a mix of uppercase letters, lowercase letters, special characters and numbers."
+        }
+        return {
+        'statusCode': 400,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        },
+        'body': json.dumps(body)
+        } 
+        
     except botocore.exceptions.ParamValidationError as e:
         if len(email) == 0:
             body = {
