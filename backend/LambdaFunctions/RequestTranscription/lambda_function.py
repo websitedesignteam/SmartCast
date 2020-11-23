b997b4f5b5bfbcafaf3a35a7e494cc4272cf193f
Ahsan Fayyaz <ahsan@iknox.com>
2020-11-23 06:56:01 +0000

    enhanced error handling for requesttranscription + added user authorization

--- backend/LambdaFunctions/RequestTranscription/lambda_function.py
+++ backend/LambdaFunctions/RequestTranscription/lambda_function.py
@@ -8,111 +8,255 @@ def convertSeconds(seconds):
     s = seconds-(h*60*60)-(m*60)
 
 
-    stringBuilder = str(h) + "Hours   " + str(m) + "Minutes   " + str(s) + "Seconds"
+    stringBuilder = str(h) + "H  " + str(m) + "M  " + str(s) + "S"
     return stringBuilder
     
     
-def putEpisode(podcastID, episodeID, episodeLength, transcribedStatus=None, transcribedText=None, tags=None, genreIDs=None, visitedCount=None, requestedEdit=None, editorsEmail=None):
+def putEpisode(podcastID, episodeID, episodeLength):
     
-    dynamoDB = boto3.resource('dynamodb')
-    table = dynamoDB.Table("PodcastTable_DEV")
-    
-    if transcribedStatus is None:
+    try: 
+        dynamoDB = boto3.resource('dynamodb')
+        table = dynamoDB.Table("PodcastTable_DEV")
+        
         transcribedStatus = "AWAITING TRANSCRIPTION APPROVAL"
-    
-    if transcribedText is None:
         transcribedText = ""
-    
-    if tags is None:
         tags = []
-        
-    if genreIDs is None:
         genreIDs = []
-        
-    if visitedCount is None:
         visitedCount = 0
-        
-    if requestedEdit is None:
         requestedEdit = ""
-    
-    if editorsEmail is None:
         editorsEmail = ""
     
     
-    #convert episodeLength into hours and minutes
-    episodeLengthConverted = convertSeconds(episodeLength)
-    
-    try:
-        table.put_item(
-            Item = {
-            'podcastID' : podcastID,
-            'episodeID' : episodeID,
-            'episodeLength' : episodeLengthConverted,
-            'transcribedStatus' : transcribedStatus,
-            'transcribedText' : transcribedText,
-            'tags' : tags,
-            'genreIDs' : genreIDs,
-            'visitedCount' : visitedCount,
-            'requestedEdit' : requestedEdit,
-            'editorsEmail' : editorsEmail
-        })
+        #convert episodeLength into hours and minutes
+        episodeLengthConverted = convertSeconds(episodeLength)
         
-        return "Success"
+        try:
+            table.put_item(
+                Item = {
+                'podcastID' : podcastID,
+                'episodeID' : episodeID,
+                'episodeLength' : episodeLengthConverted,
+                'transcribedStatus' : transcribedStatus,
+                'transcribedText' : transcribedText,
+                'tags' : tags,
+                'genreIDs' : genreIDs,
+                'visitedCount' : visitedCount,
+                'requestedEdit' : requestedEdit,
+                'editorsEmail' : editorsEmail
+            })
+            
+            return "Success"
+            
+            
+        
+        except Exception as e:
+            print("Exception : ", e)
+            body = {
+                "Error": "Could not update the podcast entry to the PodcastTable_DEV DB"
+            }
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': True,
+                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                },
+                'body': json.dumps(body)
+            }
+        
     
     except Exception as e:
         print("Exception : ", e)
-        return "Error"
+        body = {
+            "Error": "An unknown error occured"
+        }
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
     
 
 
 def lambda_handler(event, context):
     
-    body = event["body"]
-    body = json.loads(body)
     
-    
-    podcastID = str(body["podcastID"])
-    episodeID = str(body["episodeID"])
-    episodeLength = int(body["episodeLength"])
+    #----------------------------------------Retrieve the parameters from SSM store----------------------------------------#
     
     try:
-        print("here")
-        putResponse = putEpisode(podcastID, episodeID, episodeLength)
-        print("here2")
-        if putResponse == "Error":
-            return {
-                'statusCode': 404,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': True,
-                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-                },
-                'body': json.dumps({
-                    "Error": "Your request for transcription could not be completed. Try again!"
-                })
-            }
-            
-        elif putResponse == "Success":
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': True,
-                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-                },
-                'body': json.dumps({
-                    "Success": "Your request for transcription has been received. Please wait for a decision!"
-                })
-            }
-            
+        store = boto3.client('ssm')
+        env = store.get_parameter(Name = "/smartcast/env", WithDecryption = True)
+        env = json.loads(env["Parameter"]["Value"])
+        
+        AUTHORIZE_USER_LAMBDA = env["AUTHORIZE_USER_LAMBDA"]
+    
+    
     except Exception as e:
         print("Exception : ", e)
+        body = {
+            "Error": "Could not retrieve the parameters from SSM store"
+        }
         return {
-                'statusCode': 404,
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
+        
+        
+    
+    #----------------------------------------Check parameters----------------------------------------#
+
+    try:
+        #Get params from client
+        body = event["body"]
+        body = json.loads(body)
+        
+        token = str(body['access_token'])
+        podcastID = str(body['podcastID'])
+        episodeID = str(body['episodeID'])
+        episodeLength = int(body['episodeLength'])
+        
+        
+    except Exception as e:
+        print("Exception : ", e)
+        body = {
+            "Error": "You must provide a podcastID, episodeID, episodeLength, and an access_token."
+        }
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
+        
+        
+        
+    #----------------------------------------Authorize the user----------------------------------------#
+    try:
+        lambdaClient = boto3.client('lambda')
+        
+        event = {
+            "body":{
+                "access_token" : token
+            }
+        }
+        
+        event["body"] = json.dumps(event["body"])
+        response = lambdaClient.invoke(
+            FunctionName = AUTHORIZE_USER_LAMBDA,
+            Payload = json.dumps(event)
+            )
+        
+        responsePayload = response["Payload"].read()
+        responsePayload = json.loads(responsePayload)
+        if "Success" not in responsePayload:
+            if  "Something" in responsePayload["Error"]:
+                return {
+                    'statusCode': 500,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                    },
+                    'body': json.dumps(responsePayload)
+                }
+            else:
+                return {
+                    'statusCode': 400,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                    },
+                    'body': json.dumps(responsePayload)
+                }
+    except Exception as e:
+        print("Exception : ", e)
+        body = {
+            "Error": "Something went wrong. We weren't able to validate your session. Please log in again."
+        }
+        print(str(e))
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
+        
+    
+    
+    #----------------------------------------Request for transcription----------------------------------------#
+
+    try :     
+            
+        try:
+            putResponse = putEpisode(podcastID, episodeID, episodeLength)
+        
+            if putResponse == "Error":
+                return {
+                    'statusCode': 400,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                    },
+                    'body': json.dumps({
+                        "Error": "Your request for transcription could not be completed. Try again!"
+                    })
+                }
+                
+            elif putResponse == "Success":
+                return {
+                    'statusCode': 200,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                    },
+                    'body': json.dumps({
+                        "Success": "Your request for transcription has been received. Please wait for a decision!"
+                    })
+                }
+            
+        except Exception as e:
+            body = {
+                "Error": "Could not make a request at this time. Try again!"
+            }
+            print(str(e))
+            return {
+                'statusCode': 400,
                 'headers': {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
@@ -120,8 +264,24 @@ def lambda_handler(event, context):
                     'Access-Control-Allow-Credentials': True,
                     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
                 },
-                'body': json.dumps({
-                    "Exception": e
-                })
+                'body': json.dumps(body)
             }
+            
+
+    except Exception as e:
+        body = {
+            "Error": "An unknown error occured."
+        }
+        print(str(e))
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            },
+            'body': json.dumps(body)
+        }
         
