02a2bc02057e9ed9a2f534c60ef5f55169d4fd1f
Ahsan Fayyaz <ahsan@iknox.com>
2020-11-26 22:35:42 +0000

    added the edge case for handling 0 edit requests

--- backend/LambdaFunctions/getRequestedEdits/lambda_function.py
+++ backend/LambdaFunctions/getRequestedEdits/lambda_function.py
@@ -263,8 +263,25 @@ def lambda_handler(event, context):
             returnJsonObjectList.append(data)
     
     
-        return {
-                'statusCode': 200,
+        if len(returnJsonObjectList) > 0:
+            return {
+                    'statusCode': 200,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': True,
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+                    },
+                    'body': json.dumps({"Data" : returnJsonObjectList})
+            }
+        
+        else:
+            body = {
+            "Message": "There are no EDIT TRANSCRIPTION requests at the moment"
+            }
+            return {
+                'statusCode': 500,
                 'headers': {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Headers': 'Content-Type,Origin,X-Amz-Date,Authorization,X-Api-Key,x-requested-with,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
@@ -272,8 +289,8 @@ def lambda_handler(event, context):
                     'Access-Control-Allow-Credentials': True,
                     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
                 },
-                'body': json.dumps({"Data" : returnJsonObjectList})
-        }
+                'body': json.dumps(body)
+            }
     
     except Exception as e:
             print("Exception : ", e)
