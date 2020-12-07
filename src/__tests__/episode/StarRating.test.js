3bccfd50df2f9916e0f51d77862b4585cea00218
fatsoflo <flo.fong.6@gmail.com>
2020-12-07 09:08:36 -0500

    added tests without breaking app

---
+++ src/__tests__/episode/StarRating.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import StarRating from 'src/component/Episode/StarRating/StarRating';
+
+test("should render the StarRating", () => {
+    const onClick = () => {}
+    const props = {
+        onClick,
+
+    }
+    const component = shallow(<StarRating {...props} />);
+    expect(component.getElements()).toMatchSnapshot();
+});
